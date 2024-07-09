f85d43ea0b91af877e7c83f7e7327cc56c0d667e
Arshad khan <arshadkh1453@gmail.com>
2024-07-09 23:15:05 +0530

    Time: 4 ms (42.86%) | Memory: 73.2 MB (14.29%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,22 +1,18 @@
 class Solution {
-
     public double averageWaitingTime(int[][] customers) {
-        int nextIdleTime = 0;
-        long netWaitTime = 0;
+        int currenttime=0;
+        double totaltime=0.0;
+        for(int i=0;i<customers.length;i++){
+            if(currenttime<=customers[i][0]){
+                totaltime+=customers[i][1];
+                currenttime=customers[i][0]+customers[i][1];
+            }
+            else{
+                totaltime+=currenttime-customers[i][0]+customers[i][1];
+                currenttime+=customers[i][1];
 
-        for (int i = 0; i < customers.length; i++) {
-            // The next idle time for the chef is given by the time of delivery
-            // of current customer's order.
-            nextIdleTime = Math.max(customers[i][0], nextIdleTime) +
-            customers[i][1];
-
-            // The wait time for the current customer is the difference between
-            // his delivery time and arrival time.
-            netWaitTime += nextIdleTime - customers[i][0];
+            }
         }
-
-        // Divide by total customers to get average.
-        double averageWaitTime = (double) netWaitTime / customers.length;
-        return averageWaitTime;
+            return (totaltime/customers.length);
+        }
     }
-}
