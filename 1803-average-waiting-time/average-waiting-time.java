2e053045e6c4c04fa4d900903f8f575d2bcf31fd
Arshad khan <arshadkh1453@gmail.com>
2024-07-09 23:09:04 +0530

    Time: 3 ms (93.06%) | Memory: 72.8 MB (44.08%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        int currenttime=0;
+        double totaltime=0.0;
+        for(int i=0;i<customers.length;i++){
+            if(currenttime<=customers[i][0]){
+                totaltime+=customers[i][1];
+                currenttime=customers[i][0]+customers[i][1];
+            }
+            else{
+                totaltime+=currenttime-customers[i][0]+customers[i][1];
+                currenttime+=customers[i][1];
+
+            }
+        }
+            return (totaltime/customers.length);
+        }
+    }
